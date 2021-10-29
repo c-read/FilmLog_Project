35e8717098f1499163b157a2d3c00506c659ad7f
connor read <cread@softwareinstitute.com>
2021-10-29 15:39:49 +0100

    create constructors and accesors/mutators for film/actor classes

--- src/main/java/com/tsitraining/cread/FilmLog/Actor.java
+++ src/main/java/com/tsitraining/cread/FilmLog/Actor.java
@@ -1,4 +1,43 @@
 package com.tsitraining.cread.FilmLog;
 
-public class Actor {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Actor
+{
+    private String name;
+    private Date dob;
+
+
+    public Actor(String name, Date dob)
+    {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Date getDob()
+    {
+        return dob;
+    }
+    public void setDob(Date dob)
+    {
+        this.dob = dob;
+    }
+
+    List<String> filmsIn = new ArrayList<String>();
+
+    public void addFilm(Actor actor)
+    {
+        /*some logic*/
+    }
 }
--- src/main/java/com/tsitraining/cread/FilmLog/Film.java
+++ src/main/java/com/tsitraining/cread/FilmLog/Film.java
@@ -1,4 +1,43 @@
 package com.tsitraining.cread.FilmLog;
 
-public class Film {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Film
+{
+    private String name;
+    private Date releaseDate;
+
+
+    public Film(String name, Date releaseDate)
+    {
+        this.name = name;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Date getReleaseDate()
+    {
+        return releaseDate;
+    }
+    public void setDob(Date releaseDate)
+    {
+        this.releaseDate = releaseDate;
+    }
+
+    List<String> actorsIn = new ArrayList<String>();
+
+    public void addActor(Film film)
+    {
+        /*some logic*/
+    }
 }
