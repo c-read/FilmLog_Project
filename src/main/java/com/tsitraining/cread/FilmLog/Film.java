3dc5a579f8d18c8e4c6d6e90eb03f32e636dce8d
connor read <cread@softwareinstitute.com>
2021-11-26 17:30:14 +0000

    adding unit tests

--- src/main/java/com/tsitraining/cread/FilmLog/Film.java
+++ src/main/java/com/tsitraining/cread/FilmLog/Film.java
@@ -9,22 +9,40 @@ public class Film
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-/*    @Column(name = "film_id", unique = true, nullable = false)*/
     private int film_id;
-
     private String title;
+    private String description;
+    private int release_year;
     private int language_id;
+    private int rental_duration;
+    private double rental_rate;
     private int length;
-    
+    private double replacement_cost;
+    private String rating;
+    private String special_features;
 
-    /*List<String> actorsInFilm = new ArrayList<String>();*/
 
     public Film(String title, int language_id, int length)
     {
-//        this.film_id = film_id;
         this.title = title;
         this.language_id = language_id;
         this.length = length;
+    }
+
+    public Film(int film_id, String title, String description, int releaseYear, int language_id, int rental_duration,
+                double rental_rate, int length, double replacement_cost, String rating, String special_features)
+    {
+        this.film_id = film_id;
+        this.title = title;
+        this.description = description;
+        this.release_year = releaseYear;
+        this.language_id = language_id;
+        this.rental_duration = rental_duration;
+        this.rental_rate = rental_rate;
+        this.length = length;
+        this.replacement_cost = replacement_cost;
+        this.rating = rating;
+        this.special_features = special_features;
     }
 
     public Film(){}
@@ -56,4 +74,59 @@ public class Film
     public void setLength(int length) {this.length = length; }
 
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReleaseYear() {
+        return release_year;
+    }
+
+    public void setReleaseYear(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public int getRental_duration() {
+        return rental_duration;
+    }
+
+    public void setRental_duration(int rental_duration) {
+        this.rental_duration = rental_duration;
+    }
+
+    public double getRental_rate() {
+        return rental_rate;
+    }
+
+    public void setRental_rate(double rental_rate) {
+        this.rental_rate = rental_rate;
+    }
+
+    public double getReplacement_cost() {
+        return replacement_cost;
+    }
+
+    public void setReplacement_cost(double replacement_cost) {
+        this.replacement_cost = replacement_cost;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecial_features() {
+        return special_features;
+    }
+
+    public void setSpecial_features(String special_features) {
+        this.special_features = special_features;
+    }
 }
--- src/test/java/com/tsitraining/cread/FilmLog/FilmLogApplicationTests.java
+++ src/test/java/com/tsitraining/cread/FilmLog/FilmLogApplicationTests.java
@@ -6,6 +6,25 @@ import static org.junit.Assert.assertEquals;
 
 class FilmLogApplicationTests {
 	@Test
+	public void testFilmGetIdMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals("failed", 1, film.getFilm_id());
+	}
+
+	@Test
+	public void testFilmSetIdMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setFilm_id(2);
+		assertEquals("failed", 2, film.getFilm_id());
+	}
+
+	@Test
 	public void testFilmGetTitleMethod()
 	{
 		Film film = new Film("LOTR", 1, 180);
@@ -20,6 +39,24 @@ class FilmLogApplicationTests {
 		assertEquals(true,film.getTitle() == "Interstellar");
 	}
 
+	@Test
+	public void testFilmGetDescriptionMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals("failed", "description", film.getDescription());
+	}
+
+	@Test
+	public void testFilmSetDescriptionMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setDescription("desc");
+		assertEquals("failed", "desc", film.getDescription());
+	}
 
 	@Test
 	public void testFilmGetLanguageIdMethod()
@@ -50,4 +87,121 @@ class FilmLogApplicationTests {
 		film.setLength(200);
 		assertEquals(true,film.getLength() == 200);
 	}
+
+	@Test
+	public void testFilmGetReleaseYearMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals("failed", 2009, film.getReleaseYear());
+	}
+
+	@Test
+	public void testFilmSetReleaseYearMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setReleaseYear(2008);
+		assertEquals("failed", 2008, film.getReleaseYear());
+	}
+
+	@Test
+	public void testFilmGetRentalDurationMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals("failed", 24, film.getRental_duration());
+	}
+
+	@Test
+	public void testFilmSetRentalDurationMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setRental_duration(25);
+		assertEquals("failed", 25, film.getRental_duration());
+	}
+
+	@Test
+	public void testFilmGetRentalRateMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals(4.99,film.getRental_rate(),0);
+
+	}
+
+	@Test
+	public void testFilmSetRentalRateMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setRental_rate(5.99);
+		assertEquals(5.99,film.getRental_rate(),0);
+	}
+
+	@Test
+	public void testFilmGetReplacementCostMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals(9.99,film.getReplacement_cost(),0);
+	}
+
+	@Test
+	public void testFilmSetReplacementCostMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setReplacement_cost(10.99);
+		assertEquals(10.99,film.getReplacement_cost(),0);
+	}
+
+	@Test
+	public void testFilmGetRatingMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals("failed", "pg", film.getRating());
+	}
+
+	@Test
+	public void testFilmSetRatingMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setRating("pg13");
+		assertEquals("failed", "pg13", film.getRating());
+	}
+
+	@Test
+	public void testFilmGetFeaturesnMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		assertEquals("failed", "features", film.getSpecial_features());
+	}
+
+	@Test
+	public void testFilmSetFeaturesMethod()
+	{
+		Film film = new Film(1,"LOTR","description",
+				2009,1,24,4.99,
+				21,9.99,"pg","features");
+		film.setSpecial_features("more features");
+		assertEquals("failed", "more features", film.getSpecial_features());
+	}
+
+
 }
